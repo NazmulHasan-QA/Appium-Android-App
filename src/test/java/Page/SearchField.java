6827de40437cc22ac47d36199dd122c3c10c563e
Nazmul Hasan <nazmul.steadfastsqa@gmail.com>
2024-09-04 18:49:27 +0600

    Re-Fromat Code

--- src/test/java/Page/SearchField.java
+++ src/test/java/Page/SearchField.java
@@ -3,39 +3,35 @@ package Page;
 import Utils.ReusableMethods;
 import io.appium.java_client.AppiumDriver;
 
-import java.time.Duration;
-
 import static ObjectRepositories.SearchObject.*;
-import static java.awt.SystemColor.text;
 import static org.testng.Assert.assertTrue;
 
 public class SearchField {
-    public void verifySearchByTitle(AppiumDriver driver)
-    {
-        ReusableMethods.waitUntilPerformClickAction(searchButton,driver);
-        ReusableMethods.waitUntilPerformClickAction(searchBox,driver);
-        ReusableMethods.sendData(searchBox,driver,title);
-        ReusableMethods.waitUntilPerformClickAction(searchIcon,driver);
-        ReusableMethods.waitUntilPerformClickAction(ImrahimAStory,driver);
-        assertTrue(ReusableMethods.getTextFromMobileElements(storyTitle,driver).contains("ইব্রাহিম আঃ এর ঘটনা"));
+    public void verifySearchByTitle(AppiumDriver driver) {
+        ReusableMethods.waitUntilPerformClickAction(searchButton, driver);
+        ReusableMethods.waitUntilPerformClickAction(searchBox, driver);
+        ReusableMethods.sendData(searchBox, driver, title);
+        ReusableMethods.waitUntilPerformClickAction(searchIcon, driver);
+        ReusableMethods.waitUntilPerformClickAction(ImrahimAStory, driver);
+        assertTrue(ReusableMethods.getTextFromMobileElements(storyTitle, driver).contains("ইব্রাহিম আঃ এর ঘটনা"));
         driver.navigate().back();
     }
-    public void VerifySearchByCategory(AppiumDriver driver)
-    {
-        ReusableMethods.waitUntilPerformClickAction(searchBox,driver);
-        ReusableMethods.Clear(searchBox,driver);
-        ReusableMethods.sendData(searchBox,driver,category);
-        ReusableMethods.waitUntilPerformClickAction(searchIcon,driver);
-       assertTrue(ReusableMethods.getTextFromMobileElements(suraTelawat,driver).contains("তেলাওয়াত"));
-       ReusableMethods.waitUntilPerformClickAction(searchBox,driver);
-       ReusableMethods.Clear(searchBox,driver);
+
+    public void VerifySearchByCategory(AppiumDriver driver) {
+        ReusableMethods.waitUntilPerformClickAction(searchBox, driver);
+        ReusableMethods.Clear(searchBox, driver);
+        ReusableMethods.sendData(searchBox, driver, category);
+        ReusableMethods.waitUntilPerformClickAction(searchIcon, driver);
+        assertTrue(ReusableMethods.getTextFromMobileElements(suraTelawat, driver).contains("তেলাওয়াত"));
+        ReusableMethods.waitUntilPerformClickAction(searchBox, driver);
+        ReusableMethods.Clear(searchBox, driver);
 
     }
-    public  void verifySearchBySpecialCharacter(AppiumDriver driver)
-    {
-        ReusableMethods.click(searchBox,driver);
-        ReusableMethods.sendData(searchBox,driver,specialCharacter);
-        ReusableMethods.waitUntilPerformClickAction(searchIcon,driver);
-        assertTrue(ReusableMethods.waitUntilPerformIsDisplayAction(searchResult,driver),"Video found");
+
+    public void verifySearchBySpecialCharacter(AppiumDriver driver) {
+        ReusableMethods.click(searchBox, driver);
+        ReusableMethods.sendData(searchBox, driver, specialCharacter);
+        ReusableMethods.waitUntilPerformClickAction(searchIcon, driver);
+        assertTrue(ReusableMethods.waitUntilPerformIsDisplayAction(searchResult, driver), "Video found");
     }
 }
